ccc274ab189b59588374cb8c45c7f5d3f98395ce
Fitsum Kifetew <fitsumm@gmail.com>
2020-09-10 16:24:50 +0200

    added hashCode method, otherwise hashmaps/sets do not work properly

--- src/main/java/eu/fbk/se/labrecruits/LabRecruitsState.java
+++ src/main/java/eu/fbk/se/labrecruits/LabRecruitsState.java
@@ -64,14 +64,54 @@ public class LabRecruitsState implements Comparable<LabRecruitsState>{
 		return getId().compareTo(o.getId()) ;
 	}
 
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (obj instanceof LabRecruitsState) {
+//			LabRecruitsState other = (LabRecruitsState)obj;
+//			return (compareTo(other) == 0);
+//		}else {
+//			return false;
+//		}
+//	}
+
+	
+	/**
+	 * the following two methods are generated automatically by Eclipse
+	 */
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((doorId == null) ? 0 : doorId.hashCode());
+		result = prime * result + ((hasDoor == null) ? 0 : hasDoor.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (obj instanceof LabRecruitsState) {
-			LabRecruitsState other = (LabRecruitsState)obj;
-			return (compareTo(other) == 0);
-		}else {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof LabRecruitsState))
 			return false;
-		}
+		LabRecruitsState other = (LabRecruitsState) obj;
+		if (doorId == null) {
+			if (other.doorId != null)
+				return false;
+		} else if (!doorId.equals(other.doorId))
+			return false;
+		if (hasDoor == null) {
+			if (other.hasDoor != null)
+				return false;
+		} else if (!hasDoor.equals(other.hasDoor))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
